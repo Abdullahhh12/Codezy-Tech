39b5978ca7b2f6e2fe1cbe0d10621ee9f34c7607
Abdullahhh12 <97752496+Abdullahhh12@users.noreply.github.com>
2024-07-18 15:45:38 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,55 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 import Nav from './Nav'
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d9b1d28d-c194-4082-afb9-6518f4e09394");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
   return (
     <>
     <Nav/>
     <div className='bg-white text-black w-full h-screen'>
       <h1 className='flex justify-center flex-wrap pt-10 font-semibold text-3xl'> Contact Us</h1>
-      <form action="">
+      <form onSubmit={onSubmit}>
         <div className='flex justify-center items-center md:space-x-3 flex-col md:flex-row pt-10'>
 
           <div >
           <label htmlFor="">Name</label>
-          <input className='bg-white border-2 ml-2 border-black rounded' type="text"  name="" placeholder="Enter Name.." />
+          <input className='bg-white border-2 ml-2 border-black rounded' type="text"  name="" required placeholder="Enter Name.." />
           </div>
 
           <div className='pt-10 md:pt-0'> 
           <label htmlFor="">E-mail</label>
-          <input  className='bg-white border-2 ml-2 border-black rounded' type="email" name="" placeholder="Enter E-Mail.." />
+          <input  className='bg-white border-2 ml-2 border-black rounded' type="email" name="" required placeholder="Enter E-Mail.." />
           </div>
         </div>
 
         <div className='flex justify-center mt-20'>
           <label htmlFor="">Message</label>
-          <textarea name="Message" className='bg-white border-2 ml-2 border-black rounded' rows="5" id=""></textarea>
+          <textarea name="Message" className='bg-white border-2 ml-2 border-black rounded' required rows="5" id=""></textarea>
         </div>
         
         <div className='flex justify-center items-center'>
@@ -31,6 +57,7 @@ const Contact = () => {
         </div>
 
       </form>
+        <span>{result}</span>
     </div>
    
     </>
